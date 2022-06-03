79159dff408fea33d8307de455cfff2bf3c46425
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2022-06-03 20:32:29 +0530

    created legal notices and made banner() for printing the logo

---
+++ vpnla.py
@@ -0,0 +1,40 @@
+
+"""
+            █████████████████
+            ██  █████████  ██      ██        ██
+            ███  ███████  ███       ██      ██                                 ██             
+            ████  █████  ████        ██    ██      ███████     █████████       ██      ██████████
+            █████  ███  █████         ██  ██       ██    ██    ██     ██       ██      ██      ██
+            ███████   ███████          ████        ███████     ██    ███       ██      ███████ ██
+            █████████████████                      ██                                          ██
+                                                   ██
+
+    SOFTWARE: vpnla <https://github.com/lithika-damnod/vpnla>
+    AUTHOR: Lithika Damnod <https://github.com/lithika-damnod>
+
+    LICENSE: 
+        GNU General Public License version 3 (GPLv3)
+
+LEGAL NOTICE: 
+    THIS SOFTWARE IS PROVIDED FOR EDUCATIONAL USE ONLY!
+    IF YOU ENGAGE IN ANY ILLEGAL ACTIVITY
+    THE AUTHOR DOES NOT TAKE ANY RESPONSIBILITY FOR IT.
+    BY USING THIS SOFTWARE YOU AGREE WITH THESE TERMS.      
+
+"""
+
+
+def banner():
+    print(
+        """
+            █████████████████
+            ██  █████████  ██      ██        ██
+            ███  ███████  ███       ██      ██                                 ██             
+            ████  █████  ████        ██    ██      ███████     █████████       ██      ██████████
+            █████  ███  █████         ██  ██       ██    ██    ██     ██       ██      ██      ██
+            ███████   ███████          ████        ███████     ██    ███       ██      ███████ ██
+            █████████████████                      ██                                          ██
+                                                   ██
+        
+        """
+    )
